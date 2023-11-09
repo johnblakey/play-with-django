a8957b2f8e3e4a43f26c0648cd4045641e26f53d
johnblakey <4926778+johnblakey@users.noreply.github.com>
2023-11-09 10:37:50 -0800

    Removed completed TODO comment

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -19,7 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# TODO - add reference to environment variable
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', 'Specified runtime environment variable is not set.')
 
