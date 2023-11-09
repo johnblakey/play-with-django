f392f887d98c253e9390d489b3b7334c0f09cb17
johnblakey <4926778+johnblakey@users.noreply.github.com>
2023-11-09 11:00:54 -0800

    Commented out views.py auto content
    Would delete in a real project, leaving for educational deets

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+# Commented out auto-generated content - remove if a real project
+# from django.shortcuts import render
 
-# Create your views here.
+# # Create your views here.
+
+from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
