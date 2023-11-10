76efde38f0c62c6ddc32fb7eb4fe848eec9c4b92
johnblakey <4926778+johnblakey@users.noreply.github.com>
2023-11-10 07:37:07 -0800

    Change django timezone from UTC to PST
    Note that the general timezone may require the db to have
    a time change as well. Db will show UTC by default generally.
    Up to the developer to manage, django does not out of
    the box.
    https://docs.djangoproject.com/en/4.2/topics/i18n/timezones/
    https://stackoverflow.com/questions/26558422/django-and-time-zone

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -105,7 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+# https://docs.djangoproject.com/en/4.2/topics/i18n/timezones/
+# https://stackoverflow.com/questions/26558422/django-and-time-zone
+TIME_ZONE = 'PST'
 
 USE_I18N = True
 
