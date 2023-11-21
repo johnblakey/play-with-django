3fbfae3e05816f18fdc46ffa819b7a367fe395aa
johnblakey <4926778+johnblakey@users.noreply.github.com>
2023-11-20 16:08:51 -0800

    Add new polls views and urls conf

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question
 
@@ -11,8 +11,7 @@ def index(request):
     return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
+    return HttpResponse("You're looking at question %s." % question_id)
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
